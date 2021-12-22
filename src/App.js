2c507983dcb7c7922648c431bfb2a38d1fca57f4
8AREFIEV8 <7759191ai@gmail.com>
2021-12-22 13:23:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
+
+const initialValues = {
+    userName: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    type: ''
+}
 
 function App() {
+    const [userData, setUserData] = useState(initialValues);
+    const [users, setUsers] = useState([]);
+
+    const handleSubmitUser = (e) => {
+        e.preventDefault();
+        setUsers((prevState) => [...prevState, userData]);
+
+        setUserData(initialValues)
+    }
+    console.log('users', users);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+     <div className="wrapper-content">
+         <div className="table-data">
+             <table>
+                 <th>#</th>
+                 <th>USERNAME</th>
+                 <th>FIRST NAME</th>
+                 <th>LAST NAME</th>
+                 <th>EMAIL</th>
+                 <th>TYPE</th>
+
+                 <tbody>
+
+                 </tbody>
+             </table>
+         </div>
+         <div className="wrapper-inputsWithButtons">
+             <form onSubmit={handleSubmitUser}>
+                 <span>Username*</span>
+                 <input placeholder='' onChange={(e) => setUserData((prevState) =>({
+                     ...prevState,
+                     userName: e.target.value
+                 }))}
+                 value={userData.userName}
+                 />
+                 <span>First name*</span>
+                 <input placeholder='' onChange={(e) => setUserData((prevState) => ({
+                     ...prevState,
+                     firstName: e.target.value
+                 }))}
+                 value={userData.firstName}
+                 />
+                 <span>Last name*</span>
+                 <input placeholder='' onChange={(e) => setUserData((prevState) =>({
+                     ...prevState,
+                     lastName: e.target.value
+                 }))}
+                  value={userData.lastName}
+                 />
+                 <span>Email*</span>
+                 <input placeholder='' onChange={(e) => setUserData((prevState) => ({
+                     ...prevState,
+                     email: e.target.value
+                 }))}
+                 value={userData.email}
+                 />
+                 <span>Type*</span>
+                 <input placeholder='' onChange={(e) => setUserData((prevState) => ({
+                     ...prevState,
+                     type: e.target.value
+                 }))}
+                 value={userData.type}
+                 />
+
+                 <div className="buttons-wrapper">
+
+                     <button type='submit'>Create</button>
+                     <button type='reset'>Clean</button>
+
+                 </div>
+             </form>
+         </div>
+     </div>
     </div>
   );
 }
