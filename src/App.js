f5de3a06596fbc221f5997e57f5f211cf62d3730
Neolist <7759191ai@gmail.com>
2021-12-23 09:38:19 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -13,19 +13,54 @@ const initialValues = {
 function App() {
     const [userData, setUserData] = useState(initialValues);
     const [users, setUsers] = useState([]);
+    const [editableUserData, setEditableUserData] = useState({
+        isEdit: false,
+        userIndex: null
+    })
+
+    const handleRemoveClick = (index) => {
+        setUsers(users.filter((user, userIndex) => userIndex !== index))
+    }
+
+    const isFilledFields = userData.userName && userData.firstName && userData.lastName && userData.type && userData.email
 
     const handleSubmitUser = (e) => {
         e.preventDefault();
-        setUsers((prevState) => [...prevState, userData]);
 
-        setUserData(initialValues)
+        if (isFilledFields) {
+            if (editableUserData.isEdit) {
+                const editData = users;
+                editData.splice(editableUserData.userIndex, 1, userData);
+
+                setUsers(editData);
+                setEditableUserData({
+                    isEdit: false,
+                    userIndex: null
+                })
+            }else {
+                setUsers((prevState) => [...prevState, userData]);
+            }
+
+
+            setUserData(initialValues)
+        }
+
     }
+        const handleEditClick = (data, index) => {
+            setUserData(data);
+            setEditableUserData({
+                isEdit: true,
+                userIndex: index
+            })
+        }
     console.log('users', users);
 
   return (
     <div className="wrapper">
      <div className="wrapper-content">
+
          <div className="table-data">
+        <div className="buttonCreate"><button>Create</button></div>
              <table>
                  <th>#</th>
                  <th>USERNAME</th>
@@ -33,14 +68,33 @@ function App() {
                  <th>LAST NAME</th>
                  <th>EMAIL</th>
                  <th>TYPE</th>
+                 <th>ACTIONS</th>
 
                  <tbody>
+                 {users.map((user, index) => (
+                     <tr>
+                         <td>{index + 1}</td>
+                         <td>{user.userName}</td>
+                         <td>{user.firstName}</td>
+                         <td>{user.lastName}</td>
+                         <td>{user.email}</td>
+                         <td>{user.type}</td>
+
+                         <td>
+                             <div>
+                                 <button className="edit-action" onClick={() => handleEditClick(user, index)}>edit</button>
+                                 <button className="remove-action" onClick={() => handleRemoveClick(index)}>remove</button>
+                             </div>
+                         </td>
+                     </tr>
+                 ))}
 
                  </tbody>
              </table>
          </div>
          <div className="wrapper-inputsWithButtons">
              <form onSubmit={handleSubmitUser}>
+                 <strong>Create new user</strong>
                  <span>Username*</span>
                  <input placeholder='' onChange={(e) => setUserData((prevState) =>({
                      ...prevState,
@@ -79,7 +133,7 @@ function App() {
 
                  <div className="buttons-wrapper">
 
-                     <button type='submit'>Create</button>
+                     <button disabled={!isFilledFields} type='submit'>{editableUserData.isEdit ? 'Edit' : 'Create'}</button>
                      <button type='reset'>Clean</button>
 
                  </div>
